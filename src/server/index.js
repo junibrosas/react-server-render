c93694811a9014f075a875c0a778caf9d2cf0497
Juni Brosas <powerlogic1992@gmail.com>
2020-09-21 22:27:08 +0800

    setup basic redux

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import express from 'express';
 import path from 'path';
+import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import { renderToString } from 'react-dom/server';
 
 import Layout from '../shared/Layout';
+import createStore, { initializeSession } from './store';
 
 require('dotenv').config();
 
@@ -16,22 +18,29 @@ app.use(express.static(path.resolve(__dirname, '../../dist')));
 
 app.get('/*', (req, res) => {
   const context = {};
+  const store = createStore();
+
+  store.dispatch(initializeSession());
+
   const jsx = (
-    <StaticRouter context={context} location={req.url}>
-      <Layout />
-    </StaticRouter>
+    <ReduxProvider store={store}>
+      <StaticRouter context={context} location={req.url}>
+        <Layout />
+      </StaticRouter>
+    </ReduxProvider>
   );
   const reactDom = renderToString(jsx);
+  const reduxState = store.getState();
 
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(htmlTemplate(reactDom));
+  res.end(htmlTemplate(reactDom, reduxState));
 });
 
 app.listen(PORT, () => {
   console.warn(`Server is listening to port: ${PORT}`);
 });
 
-function htmlTemplate(reactDom) {
+function htmlTemplate(reactDom, reduxState) {
   return `
     <!DOCTYPE html>
     <html>
@@ -42,6 +51,9 @@ function htmlTemplate(reactDom) {
     
     <body>
       <div id="app">${reactDom}</div>
+      <script>
+        window.REDUX_DATA = ${JSON.stringify(reduxState)}
+      </script>
       <script src="./app.bundle.js"></script>
     </body>
     </html>
